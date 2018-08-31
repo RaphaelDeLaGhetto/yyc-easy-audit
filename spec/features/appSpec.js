57bbd026f9d5dddd9d6d3a2b49eae5752f1732f3
Daniel Bidulock <daniel@capitolhill.ca>
2018-08-31 14:17:31 -0600

    Sketch in first test

---
+++ spec/features/appSpec.js
@@ -0,0 +1,25 @@
+'use strict';                  
+
+const app = require('../../app'); 
+
+const Browser = require('zombie');
+const PORT = process.env.NODE_ENV === 'production' ? 3000 : 3001; 
+Browser.localhost('example.com', PORT);
+
+describe('cart', () => {
+
+  let browser;
+
+  it('does something', (done) => {
+    browser = new Browser({ waitDuration: '30s', loadCss: false });
+    browser.visit('/', (err) => {
+      if (err) {
+        done.fail(err);
+      }
+      browser.assert.success();
+      done();
+    });
+  });
+});
+
+
