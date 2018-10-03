6e8ee5a8dd9e9c19866616687c0b80eaf3f32e80
Daniel Bidulock <daniel@capitolhill.ca>
2018-10-03 15:30:51 -0600

    Get incipient client-side map resources

---
+++ public/maps/leaf-setup.js
@@ -0,0 +1,34 @@
+/**
+ * 2018-10-3 
+ * 
+ * I didn't want to have to use proprietary tiles. This article came to the
+ * rescue:
+ * 
+ * http://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap/
+ */
+
+var map = L.map('audit-map', {
+  center: [51.14880, -114.25275],
+  minZoom: 2,
+  zoom: 20 
+});
+
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: ['a', 'b', 'c']
+}).addTo(map);
+
+
+var myIcon = L.icon({
+  iconUrl: '/maps/images/pin24.png',
+  iconRetinaUrl: '/maps/images/pin48.png',
+  iconSize: [29, 24],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+})
+
+for (var i = 0; i < markers.length; ++i) {
+  L.marker([ markers[i].lat, markers[i].lng ], { icon: myIcon })
+   .bindPopup('<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>')
+   .addTo(map);
+}
