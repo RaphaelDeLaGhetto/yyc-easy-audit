80207abbb60c53417b07ce3d722ccc0f84a42c80
Daniel Bidulock <daniel@capitolhill.ca>
2018-10-04 17:15:02 -0600

    Repurpose example code

--- public/maps/leaf-setup.js
+++ public/maps/leaf-setup.js
@@ -29,6 +29,6 @@ var myIcon = L.icon({
 
 for (var i = 0; i < markers.length; ++i) {
   L.marker([ markers[i].lat, markers[i].lng ], { icon: myIcon })
-   .bindPopup('<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>')
+   .bindPopup('<address>markers[i].address</address>')
    .addTo(map);
 }
