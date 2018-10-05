edf9e7ea6075bae84c56816ffd2b2ab258c05016
Daniel Bidulock <daniel@capitolhill.ca>
2018-10-05 14:18:35 -0600

    Add sample client-rendered map

--- public/maps/leaf-setup.js
+++ public/maps/leaf-setup.js
@@ -27,8 +27,60 @@ var myIcon = L.icon({
   popupAnchor: [0, -14]
 })
 
+
+/**
+ * Event handler for marker clicks
+ */
 for (var i = 0; i < markers.length; ++i) {
+
+  function renderChart(index) {
+    var markerIndex = index;
+    return function (e) {
+      var popup = L.popup()
+          .setLatLng(e.latlng)
+          .setContent('<canvas id="chart-' + markerIndex + '" width="400" height="400"></canvas>')
+          .openOn(map);
+
+      var ctx = document.getElementById('chart-' + markerIndex).getContext('2d');
+      var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+              labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+              datasets: [{
+                  label: '# of Votes',
+                  data: [12, 19, 3, 5, 2, 3],
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255,99,132,1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero: true
+                      }
+                  }]
+              }
+          }
+      });
+    };
+  }
+
   L.marker([ markers[i].lat, markers[i].lng ], { icon: myIcon })
-   .bindPopup('<address>' + markers[i].address + '</address>')
-   .addTo(map);
+   .addTo(map).on('click', renderChart(i));
 }
