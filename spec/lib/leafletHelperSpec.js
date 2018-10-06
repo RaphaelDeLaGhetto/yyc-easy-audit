d88149ddac7f1f8d8b0d5dd3ece1f994c601ec9c
Daniel Bidulock <daniel@capitolhill.ca>
2018-10-05 19:37:00 -0600

    Organize markers by city block

--- spec/lib/leafletHelperSpec.js
+++ spec/lib/leafletHelperSpec.js
@@ -10,22 +10,28 @@ describe('leafletHelper', () => {
   const db = require('../../models');
 
   beforeEach((done) => {
-    csvImporter.importCsv('spec/data/2018-missing-data.csv', (err, arr) => {
+    csvImporter.importCsv('spec/data/2018-non-contiguous.csv', (err, arr) => {
       if (err) {
         return done.fail(err);
       }
-      expect(arr.length).toEqual(2);
+      expect(arr.length).toEqual(18);
       csvImporter.writeRecords(arr, (err, results) => {
         if (err) {
           return done.fail(err);
         }
 
-        const json = JSON.parse(fs.readFileSync('spec/data/gps.json', 'utf-8'));
-        geoJsonImporter.loadSavedGeoJson(json, (err, arr) => {
+        db.Report.introduceNeighbours((err, results) => {
           if (err) {
             return done.fail(err);
           }
-          done();
+
+          const json = JSON.parse(fs.readFileSync('spec/data/gps.json', 'utf-8'));
+          geoJsonImporter.loadSavedGeoJson(json, (err, arr) => {
+            if (err) {
+              return done.fail(err);
+            }
+            done();
+          });
         });
       });
     });
@@ -41,31 +47,37 @@ describe('leafletHelper', () => {
 
   describe('.makeMarkers', () => {
     it('makes leaflet markers object with what\'s in the database', (done) => {
-      db.Report.find({}).then((reports) => {
-        expect(reports.length).toEqual(2);
-        helper.makeMarkers((err, markers) => {
-          if (err) {
-            return done.fail(err);
-          }
-          expect(markers.length).toEqual(2);
+      helper.makeMarkers((err, markers) => {
+        if (err) {
+          return done.fail(err);
+        }
+        expect(markers.length).toEqual(2);
 
-          expect(markers[0].lat).toEqual(-12.3);
-          expect(markers[0].lng).toEqual(12.3);
-          expect(markers[0].address).toEqual('363 FAKE ST NW');
-          expect(markers[0].size).toEqual(7590);
-          expect(markers[0].assessment).toEqual(492500);
-          
-          expect(markers[1].lat).toEqual(-23.4);
-          expect(markers[1].lng).toEqual(23.4);
-          expect(markers[1].address).toEqual('351 FAKE ST NW');
-          expect(markers[1].size).toEqual(6394);
-          expect(markers[1].assessment).toEqual(463500);
+        /**
+         * These tests ensure correct data types and property definitions
+         */
+        // Block 1
+        expect(markers[0].length).toEqual(6);
+        for (const marker of markers[0]) {
+          expect(marker.lat).toEqual(-23.4);
+          expect(marker.lng).toEqual(23.4);
+          expect(marker.address).toMatch(/4\d\d FAKE ST NW/);
+          expect(marker.size).toBeGreaterThan(0);
+          expect(marker.assessment).toBeGreaterThan(0);
+        }
+
+        // Block 2
+        expect(markers[1].length).toEqual(12);
+        for (const marker of markers[1]) {
+          expect(marker.lat).toBeLessThan(0)
+          expect(marker.lng).toBeGreaterThan(12);
+          expect(marker.address).toMatch(/3\d\d FAKE ST NW/);
+          expect(marker.size).toBeGreaterThan(0);
+          expect(marker.assessment).toBeGreaterThan(0);
+        }
  
-          done();
-        });
-      }).catch((err) => {
-        done.fail(err);
-      }); 
+        done();
+      });
     });
   });
 });
