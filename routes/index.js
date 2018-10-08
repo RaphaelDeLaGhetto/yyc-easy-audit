d0b2def64c3630c0ac0257468626c57da7b626df
Daniel Bidulock <daniel@capitolhill.ca>
2018-10-08 17:26:31 -0600

    Signal the futility of it all

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // This isn't actually doing anything because of `public/index.html`
   res.render('index');
 });
 
